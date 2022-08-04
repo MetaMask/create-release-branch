049fc6896d83396a1e2e17b79f9d2841ab5496b6
Elliot Winkler <elliot.winkler@gmail.com>
2022-08-04 13:19:48 -0600

    No need to export ROOT_DIR

--- tests/functional/helpers/constants.ts
+++ tests/functional/helpers/constants.ts
@@ -1,6 +1,6 @@
 import path from 'path';
 
-export const ROOT_DIR = path.resolve(__dirname, '../../..');
+const ROOT_DIR = path.resolve(__dirname, '../../..');
 export const TOOL_EXECUTABLE_PATH = path.join(ROOT_DIR, 'src', 'cli.ts');
 export const TS_NODE_PATH = path.join(
   ROOT_DIR,
