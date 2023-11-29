46c23fbcbb9dd53a8d940c604d13592acb57f0a6
Salah-Eddine Saakoun <salah-eddine.saakoun@consensys.net>
2023-11-29 18:48:33 +0100

    refactor: extract createReleaseBranch to new workflow-opertaions file

--- src/monorepo-workflow-operations.test.ts
+++ src/monorepo-workflow-operations.test.ts
@@ -4,10 +4,7 @@ import { when } from 'jest-when';
 import { MockWritable } from 'stdio-mock';
 import { withSandbox, Sandbox, isErrorWithCode } from '../tests/helpers';
 import { buildMockProject, Require } from '../tests/unit/helpers';
-import {
-  createReleaseBranch,
-  followMonorepoWorkflow,
-} from './monorepo-workflow-operations';
+import { followMonorepoWorkflow } from './monorepo-workflow-operations';
 import * as editorModule from './editor';
 import type { Editor } from './editor';
 import * as releaseSpecificationModule from './release-specification';
@@ -1975,128 +1972,6 @@ describe('monorepo-workflow-operations', () => {
 
           expect(await fileExists(releaseSpecificationPath)).toBe(true);
         });
-      });
-    });
-  });
-
-  describe('createReleaseBranch', () => {
-    it('should create a ordinary release branch if it does not exist', async () => {
-      const project = buildMockProject();
-
-      when(jest.spyOn(repoModule, 'getCurrentBranchName'))
-        .calledWith(project.directoryPath)
-        .mockResolvedValue('main');
-
-      const newReleaseVersion = `${
-        project.releaseVersion.ordinaryNumber + 1
-      }.0.0`;
-      const newReleaseBranchName = `release/${newReleaseVersion}`;
-
-      when(jest.spyOn(repoModule, 'branchExists'))
-        .calledWith(project.directoryPath, newReleaseBranchName)
-        .mockResolvedValue(false);
-
-      const runGitCommandWithin = jest.spyOn(repoModule, 'runGitCommandWithin');
-
-      const result = await createReleaseBranch({
-        project,
-        releaseType: 'ordinary',
-      });
-
-      expect(result).toStrictEqual({
-        version: newReleaseVersion,
-        firstRun: true,
-      });
-
-      expect(runGitCommandWithin).toHaveBeenCalledWith(
-        project.directoryPath,
-        'checkout',
-        ['-b', newReleaseBranchName],
-      );
-    });
-
-    it('should create a backport release branch if it does not exist', async () => {
-      const project = buildMockProject();
-
-      when(jest.spyOn(repoModule, 'getCurrentBranchName'))
-        .calledWith(project.directoryPath)
-        .mockResolvedValue('main');
-
-      const newReleaseVersion = `${project.releaseVersion.ordinaryNumber}.${
-        project.releaseVersion.backportNumber + 1
-      }.0`;
-      const newReleaseBranchName = `release/${newReleaseVersion}`;
-
-      when(jest.spyOn(repoModule, 'branchExists'))
-        .calledWith(project.directoryPath, newReleaseBranchName)
-        .mockResolvedValue(false);
-
-      const runGitCommandWithin = jest.spyOn(repoModule, 'runGitCommandWithin');
-
-      const result = await createReleaseBranch({
-        project,
-        releaseType: 'backport',
-      });
-
-      expect(result).toStrictEqual({
-        version: newReleaseVersion,
-        firstRun: true,
-      });
-
-      expect(runGitCommandWithin).toHaveBeenCalledWith(
-        project.directoryPath,
-        'checkout',
-        ['-b', newReleaseBranchName],
-      );
-    });
-
-    it('should return existing release branch info if already checked out', async () => {
-      const project = buildMockProject();
-
-      const newReleaseVersion = `${
-        project.releaseVersion.ordinaryNumber + 1
-      }.0.0`;
-      const newReleaseBranchName = `release/${newReleaseVersion}`;
-
-      when(jest.spyOn(repoModule, 'getCurrentBranchName'))
-        .calledWith(project.directoryPath)
-        .mockResolvedValue(newReleaseBranchName);
-
-      const result = await createReleaseBranch({
-        project,
-        releaseType: 'ordinary',
-      });
-
-      expect(result).toStrictEqual({
-        version: newReleaseVersion,
-        firstRun: false,
-      });
-    });
-
-    it('should checkout existing release branch if it already exists', async () => {
-      const project = buildMockProject();
-
-      const newReleaseVersion = `${
-        project.releaseVersion.ordinaryNumber + 1
-      }.0.0`;
-      const newReleaseBranchName = `release/${newReleaseVersion}`;
-
-      when(jest.spyOn(repoModule, 'getCurrentBranchName'))
-        .calledWith(project.directoryPath)
-        .mockResolvedValue('main');
-
-      when(jest.spyOn(repoModule, 'branchExists'))
-        .calledWith(project.directoryPath, newReleaseBranchName)
-        .mockResolvedValue(true);
-
-      const result = await createReleaseBranch({
-        project,
-        releaseType: 'ordinary',
-      });
-
-      expect(result).toStrictEqual({
-        version: newReleaseVersion,
-        firstRun: false,
       });
     });
   });
--- src/monorepo-workflow-operations.ts
+++ src/monorepo-workflow-operations.ts
@@ -6,7 +6,6 @@ import {
   removeFile,
   writeFile,
 } from './fs';
-import { debug } from './misc-utils';
 import { determineEditor } from './editor';
 import { ReleaseType } from './initial-parameters';
 import {
@@ -15,17 +14,13 @@ import {
   restoreChangelogsForSkippedPackages,
 } from './project';
 import { planRelease, executeReleasePlan } from './release-plan';
-import {
-  branchExists,
-  commitAllChanges,
-  getCurrentBranchName,
-  runGitCommandWithin,
-} from './repo';
+import { commitAllChanges } from './repo';
 import {
   generateReleaseSpecificationTemplateForMonorepo,
   waitForUserToEditReleaseSpecification,
   validateReleaseSpecification,
 } from './release-specification';
+import { createReleaseBranch } from './workflow-operations';
 
 /**
  * For a monorepo, the process works like this:
@@ -156,68 +151,4 @@ export async function followMonorepoWorkflow({
     project.directoryPath,
     `Update Release ${newReleaseVersion}`,
   );
-}
-
-/**
- * Creates a new release branch in the given project repository based on the specified release type.
- *
- * @param args - The arguments.
- * @param args.project - Information about the whole project (e.g., names of
- * packages and where they can found).
- * @param args.releaseType - The type of release ("ordinary" or "backport"),
- * which affects how the version is bumped.
- * @returns A promise that resolves to an object with the new
- * release version and a boolean indicating whether it's the first run.
- */
-export async function createReleaseBranch({
-  project,
-  releaseType,
-}: {
-  project: Project;
-  releaseType: ReleaseType;
-}): Promise<{
-  version: string;
-  firstRun: boolean;
-}> {
-  const newReleaseVersion =
-    releaseType === 'backport'
-      ? `${project.releaseVersion.ordinaryNumber}.${
-          project.releaseVersion.backportNumber + 1
-        }.0`
-      : `${project.releaseVersion.ordinaryNumber + 1}.0.0`;
-
-  const releaseBranchName = `release/${newReleaseVersion}`;
-
-  const currentBranchName = await getCurrentBranchName(project.directoryPath);
-
-  if (currentBranchName === releaseBranchName) {
-    debug(`Already on ${releaseBranchName} branch.`);
-    return {
-      version: newReleaseVersion,
-      firstRun: false,
-    };
-  }
-
-  if (await branchExists(project.directoryPath, releaseBranchName)) {
-    debug(
-      `Current release branch already exists. Checking out the existing branch.`,
-    );
-    await runGitCommandWithin(project.directoryPath, 'checkout', [
-      releaseBranchName,
-    ]);
-    return {
-      version: newReleaseVersion,
-      firstRun: false,
-    };
-  }
-
-  await runGitCommandWithin(project.directoryPath, 'checkout', [
-    '-b',
-    releaseBranchName,
-  ]);
-
-  return {
-    version: newReleaseVersion,
-    firstRun: true,
-  };
 }
