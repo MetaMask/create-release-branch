e105411f5f71642c2e87fb5b2b5884bb7314aa4b
Salah-Eddine Saakoun <salah-eddine.saakoun@consensys.net>
2023-11-29 22:29:40 +0100

    fix: add more unit tests for createReleaseBranch

--- src/workflow-operations.test.ts
+++ src/workflow-operations.test.ts
@@ -78,7 +78,7 @@ describe('workflow-operations', () => {
       );
     });
 
-    it('should return existing release branch info if already checked out', async () => {
+    it('should return existing ordinary release branch info if already checked out', async () => {
       const project = buildMockProject();
 
       const newReleaseVersion = `${
@@ -101,7 +101,30 @@ describe('workflow-operations', () => {
       });
     });
 
-    it('should checkout existing release branch if it already exists', async () => {
+    it('should return existing backport release branch info if already checked out', async () => {
+      const project = buildMockProject();
+
+      const newReleaseVersion = `${project.releaseVersion.ordinaryNumber}.${
+        project.releaseVersion.backportNumber + 1
+      }.0`;
+      const newReleaseBranchName = `release/${newReleaseVersion}`;
+
+      when(jest.spyOn(repoModule, 'getCurrentBranchName'))
+        .calledWith(project.directoryPath)
+        .mockResolvedValue(newReleaseBranchName);
+
+      const result = await createReleaseBranch({
+        project,
+        releaseType: 'backport',
+      });
+
+      expect(result).toStrictEqual({
+        version: newReleaseVersion,
+        firstRun: false,
+      });
+    });
+
+    it('should checkout existing ordinary release branch if it already exists', async () => {
       const project = buildMockProject();
 
       const newReleaseVersion = `${
@@ -120,6 +143,33 @@ describe('workflow-operations', () => {
       const result = await createReleaseBranch({
         project,
         releaseType: 'ordinary',
+      });
+
+      expect(result).toStrictEqual({
+        version: newReleaseVersion,
+        firstRun: false,
+      });
+    });
+
+    it('should checkout existing backport release branch if it already exists', async () => {
+      const project = buildMockProject();
+
+      const newReleaseVersion = `${project.releaseVersion.ordinaryNumber}.${
+        project.releaseVersion.backportNumber + 1
+      }.0`;
+      const newReleaseBranchName = `release/${newReleaseVersion}`;
+
+      when(jest.spyOn(repoModule, 'getCurrentBranchName'))
+        .calledWith(project.directoryPath)
+        .mockResolvedValue('main');
+
+      when(jest.spyOn(repoModule, 'branchExists'))
+        .calledWith(project.directoryPath, newReleaseBranchName)
+        .mockResolvedValue(true);
+
+      const result = await createReleaseBranch({
+        project,
+        releaseType: 'backport',
       });
 
       expect(result).toStrictEqual({
