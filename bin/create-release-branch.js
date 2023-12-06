63f4336f2537106b3d4651e3a8137bb574a515f0
Salah-Eddine Saakoun <salah-eddine.saakoun@consensys.net>
2023-12-06 18:40:27 +0100

    fix: add .js to cli import from main script

--- bin/create-release-branch.js
+++ bin/create-release-branch.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+/* eslint-disable import/extensions */
 
-// eslint-disable-next-line import/no-unassigned-import,import/no-unresolved
-import '../dist/cli';
+// eslint-disable-next-line import/no-unassigned-import
+import '../dist/cli.js';
